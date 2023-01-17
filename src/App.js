0c1dbc0e66cac91fe02f8ff9bf0787bc258b457b
Gustavo Rojas <grojas@fastmail.com>
2023-01-17 09:03:52 -0600

    Updated the App.js with the last version

--- src/App.js
+++ src/App.js
@@ -1,223 +1,207 @@
 import * as React from 'react';
 import axios from 'axios';
 
-import './App.css';
-
-import { ReactComponent as Check } from './check.svg';
-
-const storiesReducer = (state, action) => {
-  switch (action.type) {
-    case 'STORIES_FETCH_INIT':
-      return {
-        ...state,
-        isLoading: true,
-        isError: false,
-      };
-    case 'STORIES_FETCH_SUCCESS':
-      return {
-        ...state,
-        isLoading: false,
-        isError: false,
-        data: action.payload,
-      };
-    case 'STORIES_FETCH_FAILURE':
-      return {
-        ...state,
-        isLoading: false,
-        isError: true,
-      };
-    case 'REMOVE_STORY':
-      return {
-        ...state,
-        data: state.data.filter(
-            (story) => action.payload.objectID !== story.objectID
-        ),
-      };
-    default:
-      throw new Error();
-  }
-};
-
-const useStorageState = (key, initialState) => {
-  const [value, setValue] = React.useState(
-      localStorage.getItem(key) || initialState
-
-  );
-
-  React.useEffect(() => {
-    localStorage.setItem(key, value);
-  }, [value, key]);
-
-  return [value, setValue];
-};
-
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
-const App = () => {
-  const [searchTerm, setSearchTerm] = useStorageState(
-      'search',
-      'React'
-  );
+const useSemiPersistentState = (key, initialState) => {
+    const [value, setValue] = React.useState(
+        localStorage.getItem(key) || initialState
+    );
 
-  const [url, setUrl] = React.useState(
-      `${API_ENDPOINT}${searchTerm}`
-  );
+    React.useEffect(() => {
+        localStorage.setItem(key, value);
+    }, [value, key]);
 
-  const [stories, dispatchStories] = React.useReducer(
-      storiesReducer,
-      { data: [], isLoading: false, isError: false }
-  );
+    return [value, setValue];
+};
 
-  const handleFetchStories = React.useCallback(async () => {
-    dispatchStories({ type: 'STORIES_FETCH_INIT' });
-
-    try {
-      const result = await axios.get(url);
-
-      dispatchStories({
-        type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.hits,
-      });
-    } catch {
-      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+const storiesReducer = (state, action) => {
+    switch (action.type) {
+        case 'STORIES_FETCH_INIT':
+            return {
+                ...state,
+                isLoading: true,
+                isError: false,
+            };
+        case 'STORIES_FETCH_SUCCESS':
+            return {
+                ...state,
+                isLoading: false,
+                isError: false,
+                data: action.payload,
+            };
+        case 'STORIES_FETCH_FAILURE':
+            return {
+                ...state,
+                isLoading: false,
+                isError: true,
+            };
+        case 'REMOVE_STORY':
+            return {
+                ...state,
+                data: state.data.filter(
+                    (story) => action.payload.objectID !== story.objectID
+                ),
+            };
+        default:
+            throw new Error();
     }
-  }, [url]);
+};
 
-  React.useEffect(() => {
-    handleFetchStories();
-  }, [handleFetchStories]);
+const App = () => {
+    const [searchTerm, setSearchTerm] = useSemiPersistentState(
+        'search',
+        'React'
+    );
 
-  const handleRemoveStory = (item) => {
-    dispatchStories({
-      type: 'REMOVE_STORY',
-      payload: item,
-    });
-  };
+    const [url, setUrl] = React.useState(
+        `${API_ENDPOINT}${searchTerm}`
+    );
 
-  const handleSearchInput = (event) => {
-    setSearchTerm(event.target.value);
-  };
+    const [stories, dispatchStories] = React.useReducer(
+        storiesReducer,
+        { data: [], isLoading: false, isError: false }
+    );
 
-  const handleSearchSubmit = (event) => {
-    setUrl(`${API_ENDPOINT}${searchTerm}`);
+    const handleFetchStories = React.useCallback(async () => {
+        dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    event.preventDefault();
-  };
+        try {
+            const result = await axios.get(url);
 
-  return (
-      <div className="container">
-        <h1 className="headline-primary">My Hacker Stories</h1>
+            dispatchStories({
+                type: 'STORIES_FETCH_SUCCESS',
+                payload: result.data.hits,
+            });
+        } catch {
+            dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+        }
+    }, [url]);
 
-        <SearchForm
-            searchTerm={searchTerm}
-            onSearchInput={handleSearchInput}
-            onSearchSubmit={handleSearchSubmit}
-        />
+    React.useEffect(() => {
+        handleFetchStories();
+    }, [handleFetchStories]);
 
-        <hr />
+    const handleRemoveStory = (item) => {
+        dispatchStories({
+            type: 'REMOVE_STORY',
+            payload: item,
+        });
+    };
 
-        {stories.isError && <p>Something went wrong ...</p>}
+    const handleSearchInput = (event) => {
+        setSearchTerm(event.target.value);
+    };
 
-        {stories.isLoading ? (
-            <p>Loading ...</p>
-        ) : (
-            <List list={stories.data} onRemoveItem={handleRemoveStory} />
-        )}
-      </div>
-  );
+    const handleSearchSubmit = (event) => {
+        setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+        event.preventDefault();
+    };
+
+    return (
+        <div>
+            <h1>My Hacker Stories</h1>
+
+            <SearchForm
+                searchTerm={searchTerm}
+                onSearchInput={handleSearchInput}
+                onSearchSubmit={handleSearchSubmit}
+            />
+
+            <hr />
+
+            {stories.isError && <p>Something went wrong ...</p>}
+
+            {stories.isLoading ? (
+                <p>Loading ...</p>
+            ) : (
+                <List list={stories.data} onRemoveItem={handleRemoveStory} />
+            )}
+        </div>
+    );
 };
 
 const SearchForm = ({
-                      searchTerm,
-                      onSearchInput,
-                      onSearchSubmit,
+                        searchTerm,
+                        onSearchInput,
+                        onSearchSubmit,
                     }) => (
-    <form onSubmit={onSearchSubmit} className="search-form">
-      <InputWithLabel
-          id="search"
-          value={searchTerm}
-          isFocused
-          onInputChange={onSearchInput}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
+    <form onSubmit={onSearchSubmit}>
+        <InputWithLabel
+            id="search"
+            value={searchTerm}
+            isFocused
+            onInputChange={onSearchInput}
+        >
+            <strong>Search:</strong>
+        </InputWithLabel>
 
-      <button
-          type="submit"
-          disabled={!searchTerm}
-          className="button button_large"
-      >
-        Submit
-      </button>
+        <button type="submit" disabled={!searchTerm}>
+            Submit
+        </button>
     </form>
 );
 
 const InputWithLabel = ({
-                          id,
-                          value,
-                          type = 'text',
-                          onInputChange,
-                          isFocused,
-                          children,
+                            id,
+                            value,
+                            type = 'text',
+                            onInputChange,
+                            isFocused,
+                            children,
                         }) => {
-  const inputRef = React.useRef();
+    const inputRef = React.useRef();
 
-  React.useEffect(() => {
-    if (isFocused && inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [isFocused]);
+    React.useEffect(() => {
+        if (isFocused && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [isFocused]);
 
-  return (
-
-      <>
-        <label htmlFor={id} className="label">
-                {children}
-        </label>
-        &nbsp;
-        <input
-            ref={inputRef}
-            id={id}
-            type={type}
-            value={value}
-            onChange={onInputChange}
-            className="input"
-        />
-      </>
-  );
+    return (
+        <>
+            <label htmlFor={id}>{children}</label>
+            &nbsp;
+            <input
+                id={id}
+                ref={inputRef}
+                type={type}
+                value={value}
+                onChange={onInputChange}
+            />
+        </>
+    );
 };
 
 const List = ({ list, onRemoveItem }) => (
     <ul>
-      {list.map((item) => (
-          <Item
-              key={item.objectID}
-              item={item}
-              onRemoveItem={onRemoveItem}
-          />
-      ))}
+        {list.map((item) => (
+            <Item
+                key={item.objectID}
+                item={item}
+                onRemoveItem={onRemoveItem}
+            />
+        ))}
     </ul>
 );
 
 const Item = ({ item, onRemoveItem }) => (
-    <li className="item">
-    <span style={{ width: '40%' }}>
+    <li>
+    <span>
       <a href={item.url}>{item.title}</a>
     </span>
-      <span style={{ width: '30%' }}>{item.author}</span>
-      <span style={{ width: '10%' }}>{item.num_comments}</span>
-      <span style={{ width: '10%' }}>{item.points}</span>
-      <span style={{ width: '10%' }}>
-      <button
-          type="button"
-          onClick={() => onRemoveItem(item)}
-          className="button button_small"
-      >
-        <Check height="18px" width="18px" />
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
       </button>
     </span>
     </li>
 );
 
 export default App;
-export {storiesReducer,SearchForm,InputWithLabel,List,Item};
+
+export { storiesReducer, SearchForm, InputWithLabel, List, Item };
